165fe5eb529c1645fa020ccc7d17e05877bcbd68
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-29 12:13:28 +0200

    feat: sass runs

---
+++ la-mia-pizzeria-static/gulpfile.js
@@ -0,0 +1,39 @@
+﻿/*
+OLD SYNTAX
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+
+gulp.task('sass', function () {
+    return gulp.src('./wwwroot/css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css/compiled'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./wwwroot/css/*.scss', gulp.series('sass'));
+});
+*/
+
+///////////////// BEST PRACTICE functional syntax /////////////////
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// Define the 'sass' task
+function compileSass(cb) {
+    return src('./wwwroot/css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./wwwroot/css/compiled'));
+}
+
+// Define the 'watch' task
+function watchSass(cb) {
+    watch('./wwwroot/css/*.scss', series(compileSass));
+    cb();
+}
+
+// Export tasks
+
+exports.compileSass = compileSass;
+exports.watchSass = watchSass;
+exports.default = series(compileSass, watchSass);
